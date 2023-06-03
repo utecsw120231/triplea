632b2aa35f758edb0a975c31c005fcb0a6922425
Gabrieleeh32159 <85197211+Gabrieleeh32159@users.noreply.github.com>
2023-06-02 20:16:18 -0500

    entrega2

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
+import { Route, Routes, Navigate, Link } from 'react-router-dom'
 import LoginForm from "./components/LoginForm"
 import MainPage from "./components/MainPage"
+import RegisterForm from "./components/RegisterForm"
 
 function App() {
-
   const [user, setUser] = useState(null)
-
   return (
     <React.Fragment>
       <div className="p-0 bg-[url('/background.jpg')] bg-no-repeat bg-cover h-screen" >
-        {user ? <MainPage user={user}></MainPage> : <LoginForm setUser={setUser}></LoginForm>}
+        <Routes>
+          <Route path="/" element={user ? <MainPage user={user}/> : <Navigate to='/login'/>}/>
+          <Route path='/register' element={<RegisterForm />} />
+          <Route path='/login' element={user ? <Navigate to='/'/> : <LoginForm setUser={setUser}/> }/>
+        </Routes>
       </div>
     </React.Fragment>
   )
