eb27dfcd8661f9118d87e300c3a6746d2edb5e3f
Alvaro Guerrero Jiménez <alvaro.guerrero@utec.edu.pe>
2023-06-16 09:17:00 -0500

    feat(db/schema): Add `style`

--- StoryTeller/schema.sql
+++ StoryTeller/schema.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS story CASCADE;
 DROP TABLE IF EXISTS image CASCADE;
+DROP TABLE IF EXISTS style CASCADE;
 
 CREATE TABLE story (
   story_id SERIAL PRIMARY KEY,
@@ -12,4 +13,10 @@ CREATE TABLE image (
   hash TEXT PRIMARY KEY,
   story_id INTEGER NOT NULL,
   FOREIGN KEY (story_id) REFERENCES story(story_id)
+);
+
+CREATE TABLE style (
+  user_email TEXT NOT NULL,
+  style TEXT NOT NULL,
+  PRIMARY KEY (user_email, style)
 );
